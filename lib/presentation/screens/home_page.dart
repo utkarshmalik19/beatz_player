ba2544b794325fa13f9b607db7db2322c37bf9ed
utkarshmalik19 <utkarshmalik19@gmail.com>
2023-11-09 00:18:52 +0530

    song pause and resume logic added

--- lib/presentation/screens/home_page.dart
+++ lib/presentation/screens/home_page.dart
@@ -5,11 +5,9 @@ import 'package:beatz_player/presentation/widgets/bottom_music_bar.dart';
 import 'package:beatz_player/presentation/widgets/song_list.dart';
 import 'package:beatz_player/utils/consts.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_media_metadata/flutter_media_metadata.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:path/path.dart';
-
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -19,7 +17,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
- // final FlutterAudioQuery audioQuery = FlutterAudioQuery();
   List<Song> songs = [];
   AudioPlayer audioPlayer = AudioPlayer();
   int selectedSongIndex = -1; // Keep track of the selected song index
@@ -30,39 +27,34 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> fetchAudioFiles() async {
-  
-  PermissionStatus status = await Permission.storage.request();
+    PermissionStatus status = await Permission.storage.request();
 
-  if (status.isGranted) {
-    List<String> audioExtensions = [
-      '.mp3',
-      '.m4a',
-      '.flac'
-    ];
-    List<FileSystemEntity> audioFiles = [];
-    Directory directory = Directory('/storage/emulated/0/Music'); // Update this path to the desired directory
+    if (status.isGranted) {
+      List<String> audioExtensions = ['.mp3', '.m4a', '.flac'];
+      List<FileSystemEntity> audioFiles = [];
+      Directory directory = Directory(
+          '/storage/emulated/0/Music'); // Update this path to the desired directory
 
-    if (directory.existsSync()) {
-      await _findAudioFiles(directory, audioExtensions, audioFiles);
+      if (directory.existsSync()) {
+        await _findAudioFiles(directory, audioExtensions, audioFiles);
+      } else {
+        print("Directory does not exist: ${directory.path}");
+      }
+
+      for (var file in audioFiles) {
+        // Extract song information from the file and create a Song object
+        songs.add(Song(
+          title: basenameWithoutExtension(file.path),
+          artist:
+              'Unknown Artist', // You can fetch artist information if available
+          filePath: file.path,
+        ));
+      }
+      setState(() {});
     } else {
-      print("Directory does not exist: ${directory.path}");
+      print("Permission denied");
     }
-
-    for (var file in audioFiles) {
-      
-      // Extract song information from the file and create a Song object
-      songs.add(Song(
-        title: basenameWithoutExtension(file.path),
-        artist: 'Unknown Artist', // You can fetch artist information if available
-        filePath: file.path,
-      ));
-    }
-    setState(() {});
-  } else {
-    print("Permission denied");
   }
-}
-
 
   Future<void> _findAudioFiles(Directory directory, List<String> extensions,
       List<FileSystemEntity> audioFiles) async {
@@ -77,41 +69,50 @@ class _HomePageState extends State<HomePage> {
       }
     }
   }
-Future<void> playSong(int index) async {
+
+  Future<void> playSong(int index) async {
     // Check if the selected song index is different from the currently playing song
     if (selectedSongIndex != index) {
       // Stop the currently playing song (if any)
       await audioPlayer.stop();
 
       // Play the new selected song
-      await audioPlayer.play(UrlSource(songs[index].filePath)); // Assuming songs contain file paths
+      await audioPlayer.play(UrlSource(
+          songs[index].filePath)); // Assuming songs contain file paths
 
       // Update the selected song index
       setState(() {
         selectedSongIndex = index;
       });
     } else {
-      // If the same song is tapped again, pause or resume it
-      // if (audioPlayer.state == PlayerState.PLAYING) {
-      //   await audioPlayer.pause();
-      // } else if (audioPlayer.state == PlayerState.PAUSED) {
-      //   await audioPlayer.resume();
-      // }
+      //If the same song is tapped again, pause or resume it
+      if (audioPlayer.state == PlayerState.playing) {
+        await audioPlayer.pause();
+      } else if (audioPlayer.state == PlayerState.paused) {
+        await audioPlayer.resume();
+      }
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppConstants.bgColor,
       appBar: AppBar(
-        title: Text('Beatz'),
+        backgroundColor: AppConstants.bgColor,
+        elevation: 0,
+        title: Text(
+          'Beatz Music Player',
+          style: AppConstants.headerTextStyle,
+        ),
       ),
       body: songs.isEmpty
           ? const Center(child: CircularProgressIndicator())
           : SongList(songs: songs, playSong: playSong),
-          bottomNavigationBar: BottomMusicBar(currentSongTitle: songs[selectedSongIndex].title,currentSongArtist: songs[selectedSongIndex].artist),
+      bottomNavigationBar: BottomMusicBar(
+          currentSongTitle: songs[selectedSongIndex].title,
+          currentSongArtist: songs[selectedSongIndex].artist,
+          audioPlayer: audioPlayer,),
     );
   }
-  
-
 }
--- lib/presentation/widgets/bottom_music_bar.dart
+++ lib/presentation/widgets/bottom_music_bar.dart
@@ -1,14 +1,35 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:beatz_player/utils/consts.dart';
 import 'package:flutter/material.dart';
 
-class BottomMusicBar extends StatelessWidget {
+class BottomMusicBar extends StatefulWidget {
   final String currentSongTitle;
   final String currentSongArtist;
+  final AudioPlayer audioPlayer;
   const BottomMusicBar(
       {super.key,
       required this.currentSongTitle,
-      required this.currentSongArtist});
+      required this.currentSongArtist,
+      required this.audioPlayer});
 
+  @override
+  State<BottomMusicBar> createState() => _BottomMusicBarState();
+}
+
+class _BottomMusicBarState extends State<BottomMusicBar> {
+    final ValueNotifier<PlayerState> _playerStateNotifier = ValueNotifier(PlayerState.paused);
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+     // Listen to changes in the player state
+    widget.audioPlayer.onPlayerStateChanged.listen((PlayerState state) {
+      
+      setState(() {
+        _playerStateNotifier.value = state;
+      });
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -25,14 +46,30 @@ class BottomMusicBar extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Icon(Icons.music_note,
-                  color: Colors.white), // Add a music note icon      
-                  SizedBox(width: 20,),
+                  color: Colors.white), // Add a music note icon
+              SizedBox(
+                width: 20,
+              ),
               Text(
-                currentSongTitle,
+                widget.currentSongTitle,
                 style: TextStyle(color: Colors.white, fontSize: 16),
               ),
               Spacer(),
-              Icon(Icons.play_arrow,color: Colors.white,)
+              IconButton(
+                icon: Icon(
+                  _playerStateNotifier.value == PlayerState.playing
+                    ? Icons.pause
+                    : Icons.play_arrow,
+                ),
+                    onPressed: () {
+                      if (_playerStateNotifier.value == PlayerState.playing) {
+                    widget.audioPlayer.pause();
+                  } else {
+                    widget.audioPlayer.resume();
+                  }
+                    },
+                color: Colors.white,
+              )
             ],
           ),
         ),
