b303decb767bf897018e695d732768d93607eb83
utkarshmalik19 <utkarshmalik19@gmail.com>
2023-11-12 18:04:07 +0530

    skip to next song logic added

--- lib/presentation/screens/current_song_screen.dart
+++ lib/presentation/screens/current_song_screen.dart
@@ -6,17 +6,18 @@ class CurrentSongScreen extends StatefulWidget {
   final AudioPlayer audioPlayer;
   final bool isPlaying;
 
-  const CurrentSongScreen({super.key, required this.audioPlayer, required this.isPlaying});
+  const CurrentSongScreen(
+      {super.key, required this.audioPlayer, required this.isPlaying});
   @override
   State<CurrentSongScreen> createState() => _CurrentSongScreenState();
 }
 
 class _CurrentSongScreenState extends State<CurrentSongScreen> {
-     late bool isPlaying ;
-     late double sliderValue;
-     Duration? duration;
+  late bool isPlaying;
+  late double sliderValue;
+  Duration? duration;
 
-    @override
+  @override
   void initState() {
     super.initState();
     // Listen to changes in the player state
@@ -27,19 +28,21 @@ class _CurrentSongScreenState extends State<CurrentSongScreen> {
         isPlaying = state == PlayerState.playing;
       });
     });
-     widget.audioPlayer.onPositionChanged.listen((Duration duration) {
+    widget.audioPlayer.onPositionChanged.listen((Duration duration) {
       setState(() {
         sliderValue = duration.inMilliseconds.toDouble();
       });
     });
-     _initializeDuration();
+    _initializeDuration();
   }
+
   Future<void> _initializeDuration() async {
     final d = await widget.audioPlayer.getDuration();
     setState(() {
       duration = d;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,22 +58,22 @@ class _CurrentSongScreenState extends State<CurrentSongScreen> {
             // Image placeholder (replace with album art)
             SizedBox(
                 height: 300, child: Image.asset('assets/images/music.png')),
-                
+
             // Progress bar, play/pause button, next song, previous song buttons
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              child: Slider(
-                value: sliderValue,
-                min: 0.0,                
-                max: duration?.inMilliseconds.toDouble() ?? 0.0,
-                onChanged: (double value){
-                  setState(() {
-                    sliderValue = value;
-                  });
-                  widget.audioPlayer.seek(Duration(milliseconds: value.toInt()));
-                },
-              )
-            ),
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Slider(
+                  value: sliderValue.clamp(0.0, (duration?.inMilliseconds.toDouble() ?? 1.0)),
+                  min: 0.0,
+                  max: (duration?.inMilliseconds.toDouble() ?? 0.0),
+                  onChanged: (double value) {
+                    setState(() {
+                      sliderValue = value;
+                    });
+                    widget.audioPlayer
+                        .seek(Duration(milliseconds: value.toInt()));
+                  },
+                )),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -83,26 +86,31 @@ class _CurrentSongScreenState extends State<CurrentSongScreen> {
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 60),
                   child: IconButton(
-                  icon: Icon(
-                    isPlaying ==true ? Icons.pause : Icons.play_arrow,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      
-                      if (isPlaying == true) {
+                    icon: Icon(
+                      isPlaying == true ? Icons.pause : Icons.play_arrow,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        if (isPlaying == true) {
                           widget.audioPlayer.pause();
                         } else {
                           widget.audioPlayer.resume();
                         }
-                    });
-                  },
+                      });
+                    },
+                  ),
                 ),
-                ),
-                 IconButton(
+                IconButton(
                   icon: Icon(Icons.skip_next, color: Colors.white),
                   onPressed: () {
                     // Implement logic for skipping to the next song
+                    // Get the duration of the current song
+                    widget.audioPlayer.getDuration().then((duration) {
+                      // Seek to the end of the current song
+                      widget.audioPlayer.seek(
+                          Duration(milliseconds: duration!.inMilliseconds - 1));
+                    });
                   },
                 ),
               ],
